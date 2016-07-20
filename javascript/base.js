31cb6241848b3e8ac97b7e4df76bbdcf2a8b59cc
Ma WuJi <295614927@qq.com>
2016-07-20 13:48:40 +0800

    1

---
+++ javascript/base.js
@@ -0,0 +1,32 @@
+// Window Scroll
+var windowScroll = function () {
+    $(window).scroll(function () {
+
+        var scrollPos = $(this).scrollTop();
+
+        if ($(window).scrollTop() > 100) {
+            $('.headerblock').addClass('headerblock-scrolltop');
+        } else {
+            $('.headerblock').removeClass('headerblock-scrolltop');
+        }
+    });
+};
+
+$(document).ready(function() {
+    windowScroll();
+});
+
+var scrollHeaderEvent = function(){
+    if ($(window).scrollTop() > 70) {
+            $('.site-header').addClass('site-header-nav-scrolled');
+        } else {
+            $('.site-header').removeClass('site-header-nav-scrolled');
+        }
+}
+var scrollCatelogEvent = function(){
+    if ($(window).scrollTop() > 70) {
+            $('.site-header').addClass('site-header-nav-scrolled');
+        } else {
+            $('.site-header').removeClass('site-header-nav-scrolled');
+        }
+}
