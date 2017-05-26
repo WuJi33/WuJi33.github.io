a3b74334f26c4c0d9ee9c320ba73f216a9b8a505
马无忌 <mawjd@yonyou.com>
2017-05-26 14:10:30 +0800

    a

--- javascript/base.js
+++ javascript/base.js
@@ -1,6 +1,8 @@
 //globle variable
 var lastTimeScrollTop;
 var fromAddr = "/";
+var photosInfoPos = [];
+var photosInfoPosphotosInfoNag = [];
 
 $(document).ready(function() {
     windowScroll();
@@ -10,6 +12,16 @@ $(document).ready(function() {
     $(".glyphicon-arrow-left").each(function() {
         $(this).click(goBack);
     })
+
+    $.getJSON("/photoalbum/imginfo.json", function(rs) {
+        photosInfoPos = rs;
+        photosInfoPos.sort(function(a, b) {
+            return a.basename > b.basename;
+        });
+        photosInfoNag = photosInfoPos.concat([]);
+        photosInfoNag.reverse();
+    });
+
     window.onresize = resize;
     resize();
 });
@@ -61,30 +73,83 @@ function dropdownmenu() {
         $("#dropdownmenu").hide();
     }
 }
-var closeImage=function(){
+var closeImage = function() {
     $(".photos-cover").hide();
     $(".photos-bigshow").hide();
 }
 
 var openImage = function(trigger) {
-    var img = $(trigger).find("img").first(); 
+    var imgsrc = "";
+    if (typeof(trigger) == "object") {
+        imgsrc = $(trigger).find("img").first().prop("src");
+    } else if (typeof(trigger) == "string") {
+        imgsrc = "/photoalbum/photos/" + trigger;
+    }
+
     var imgCanvas = $(".photos-bigshow");
-    imgCanvas.find("img").first().prop("outerHTML", img.prop("outerHTML"));
-    
-    imgCanvas.find("img").first().attr("onload",function() {
+    var newImg = imgCanvas.find("img").first();
+    newImg.prop("src", imgsrc);
+
+
+    photosInfoPos.forEach(function(ele) {
+        if (imgsrc.indexOf(ele.filename) > -1) {
+            $(".photo-bottomdesc").text(ele.desc);
+            newImg.prop("name", ele.basename);
+        }
+    }, this);
+
+    newImg.attr("onload", function() {
         setTimeout(function() {
             resize();
-            imgCanvas.show();  
-            $(".photos-cover").show();      
-        }, 100);        
+            imgCanvas.show();
+            $(".photos-cover").show();
+        }, 100);
     });
 }
 var resize = function() {
     $(".photos-bigshow").css("top", $(window).height() / 2 - $(".photos-bigshow").height() / 2);
     if ($(window).width() > 992) {
         $(".photos-bigshow").css("left", $(window).width() / 2 - $(".photos-bigshow").width() / 2);
-    }else{
-        $(".photos-bigshow").css("left",0);
+    } else {
+        $(".photos-bigshow").css("left", 0);
+    }
+    // $(".photos-left").height($(".photos-bigshow").height() - $(".photo-bottomdesc").height());
+    // $(".photos-right").height($(".photos-bigshow").height() - $(".photo-bottomdesc").height());
+
+}
+
+var changeImg = function(tr, dir, e) {
+    $(".photos-bigshow").hide();
+    var img = $(tr).siblings("img").attr("name");
+    var find = false;
+    var imgFilName = "";
+    var ary = null;
+    var inforList = null;
+    if (dir == 1) {
+        inforList = photosInfoNag;
+    } else {
+        inforList = photosInfoPos;
+    }
+    inforList.forEach(function(item) {
+        if (find && imgFilName == "") {
+            imgFilName = item.filename;
+        }
+        if (item.basename == img) {
+            find = true;
+        }
+
+    })
+    if (imgFilName != "") {
+        openImage(imgFilName);
+    } else {
+        $(".photos-bigshow").show();
+        alert("已经没有了！");
+    }
+    e = e || window.event;
+    if (e.stopPropagation) { //W3C阻止冒泡方法  
+        e.stopPropagation();
+    } else {
+        e.cancelBubble = true; //IE阻止冒泡方法  
     }
 
 }
